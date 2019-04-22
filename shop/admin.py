52eede580c76ce6838ec1d8234aed3ad692b3b78
severisc <GgitHub01>
2019-04-22 21:54:50 +0300

    adaugat admin si shop

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+# Register your models here.
+
+from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Category
+
+admin.site.register(Product)
+admin.site.register(Category)
+
+
